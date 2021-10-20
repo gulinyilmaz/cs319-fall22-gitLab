204dad374b04d8744871ca8440fed2327ebf0b23
Gülin Yılmaz <64426686+gulinyilmaz@users.noreply.github.com>
2021-10-20 19:46:55 +0300

    refactor

--- src/Util.py
+++ src/Util.py
@@ -1,8 +1,12 @@
 from enum import Enum
 
+<<<<<<< HEAD
 # vibranium is specific to Steve's car
 
 
 materialType = Enum('Titanium', 'Carbon', 'Vibranium')
 
+=======
+materialType = Enum('Titanium', 'Carbon', 'AlloyX')
+>>>>>>> bruce-alloyX
 
